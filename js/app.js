66ec1724c31e30aae319718e56f00f5f0aa66d7a
skimanikings <kimanisimon856@gmail.com>
2019-11-02 13:20:37 +0300

    add text headings to the project

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 let submit = function() {
-    event.preventDefault();
-    var name=document.getElementById("name")
-    var email = document.getElementById("email")
-    var message = document.getElementById("message")
+    var name=document.getElementById("name");
+    var email = document.getElementById("email");
+    var message = document.getElementById("message");
+    var display=document.getElementById("display").innerHTML;
 
     if (name ==" " );  (email== " "); (message == ""); {
- alert("Enter valid information");
-    };
-}
+     alert("Please enter valid information");
+    }
+    else {
+        display.style.background="green"
+        display.style.color="white"
+display.innerHTML="Hey " + name + " your message has been received ." + " Thank you!"
+    }
+};
